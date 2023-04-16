39daf4a4d95e2f039c97a7f4f332f6e093023b05
Ohad Cohen <ohad909881122@gmail.com>
2023-04-16 15:25:43 +0300

    U

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { auth } from "../firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { BiUser } from "react-icons/bi";
 import { AiFillLock, AiOutlineMail } from "react-icons/ai";
 import { toast } from "react-hot-toast";
+import GoogleButton from "react-google-button";
 
 const SignUp = () => {
   const router = useRouter();
@@ -20,30 +21,39 @@ const SignUp = () => {
         console.log(userCredential);
         router.push("/SignIn");
         toast.success("User sign Up:");
-
       })
       .catch((error) => {
         console.log(error);
         toast.error("User sign Up Failed:");
-
       });
   };
 
+  const googleProvider = new GoogleAuthProvider();
+
+  const signInWithGoogle = () =>{
+      signInWithPopup(auth,googleProvider).then(res=>{
+        toast.success("User sign Up:");
+        router.push("/Home");
+      }) .catch((error) => {
+        console.log(error);
+        toast.error("User sign Up Failed:");
+      });
+  }
   return (
     <div className="container">
       <div className="form signup">
-          <h1 style={{color:'white'}}>Create Account</h1>
-          <div className="inputBox">
+        <h1 style={{ color: "white" }}>Create Account</h1>
+        <div className="inputBox">
           <input
             type="email"
             placeholder="Enter your email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-           <i>
-              <AiOutlineMail />
-            </i>
-            <span>Email</span>
+          <i>
+            <AiOutlineMail />
+          </i>
+          <span>Email</span>
         </div>
 
         <div className="inputBox">
@@ -54,19 +64,22 @@ const SignUp = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
           <i>
-          <AiFillLock />
-            </i>
-            <span>Username</span>
-            </div>
+            <AiFillLock />
+          </i>
+          <span>Username</span>
+        </div>
+        <p>OR</p>
+        <GoogleButton style={{width:'70%',outline:'none'}} onClick={signInWithGoogle}/>
 
-        <button  className="btn" onClick={signUp}>Sign Up</button>
+        <button className="btn" onClick={signUp}>
+          Sign Up
+        </button>
+        
         <p>
-            Already a member ? <a href="SignIn">Log in</a>
-          </p>
+          Already a member ? <a href="SignIn">Log in</a>
+        </p>
       </div>
-      </div>
-    
-
+    </div>
   );
 };
 
